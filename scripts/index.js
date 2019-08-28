7e15ee95e5c32b7c3b502bf7bb8383a8c69f7c7f
Alexander Fukui <alexander.fukui@gmail.com>
2019-08-28 10:26:19 -0500

    add gitignore file

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+'use strict';
+/* global shoppingList, store, api */
+
+$(document).ready(function() {
+  shoppingList.bindEventListeners();
+
+  // On initial load, fetch Shopping Items and render
+  api.getItems()
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    })
+    .catch(err => console.log(err.message));
+});
+
